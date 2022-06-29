f1c4a033453fafcc6859008afe73fc20fce32cf7
northcoders <northcoders@northcoderss-iMac-2.local>
2022-06-29 12:27:01 +0100

    add solutions

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
+const { primesTo1000 } = require('./primes');
+
+// brute force approach
 function smallestCommonMultipleBrute(nums) {
-  let result = 1;
+  let result = Math.max(...nums);
   for (let i = 0; i < nums.length; i++) {
+    // console.log(nums[i], result, result % nums[i] !== 0);
     if (result % nums[i] !== 0) {
       result++;
       i = 0;
+    }
+    // account for unsorted inputs
+    if (i === nums.length - 1) {
+      if (nums.some((num) => result % num !== 0)) i = -1;
     }
   }
   return result;
 }
 
-module.exports = { smallestCommonMultipleBrute };
+// quick version (warning: contains poorly explained maths)
+function smallestCommonMultiplePrimes(nums) {
+  // get prime factors of every number
+  const primeFactors = nums.map((num) => getPrimeFactors(num));
+  // reduce to find most common primes
+  return primeFactors.reduce((acc, factors, i) => {
+    // take the highest common primes
+    for (let prime in factors) {
+      if (factors[prime] > acc[prime] || !acc[prime]) {
+        acc[prime] = factors[prime];
+      }
+    }
+    // find total produce of prime factors on last iteration
+    if (i === primeFactors.length - 1) {
+      return Object.entries(acc).reduce(
+        (total, [prime, exponent]) => total * prime ** exponent,
+        1
+      );
+    }
+    return acc;
+  }, {});
+}
+
+/*
+Returns a map of prime factors of a number
+e.g. 12 is factored into 2 * 2 * 3
+returns { 2: 2, 3: 1 }
+*/
+function getPrimeFactors(num) {
+  const primeFactors = [];
+  // limited by list of known primes. Using < 1000 as an example
+  const primes = primesTo1000;
+  let i = 0;
+  while (num > 1 && i < primes.length) {
+    if (num % primes[i] === 0) {
+      primeFactors.push(primes[i]);
+      num = num / primes[i];
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return primeFactors.reduce((acc, factor) => {
+    acc[factor] = factor in acc ? acc[factor] + 1 : 1;
+    return acc;
+  }, {});
+}
+
+module.exports = { smallestCommonMultipleBrute, smallestCommonMultiplePrimes };
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,27 +1,71 @@
 const { expect } = require('chai');
-const { smallestCommonMultipleBrute } = require('../index');
+const fs = require('fs/promises');
+const {
+  smallestCommonMultipleBrute,
+  smallestCommonMultiplePrimes,
+} = require('../index');
 const { PerformanceObserver, performance } = require('perf_hooks');
 // https://nodejs.org/api/perf_hooks.html#performance-measurement-apis
 
-describe('smallestCommonMultiple', () => {
-  it('returns the single number from a set of one', () => {
-    const testSet = [1];
-    expect(smallestCommonMultipleBrute(testSet)).to.equal(1);
+runTests(smallestCommonMultipleBrute, 'smallestCommonMultipleBrute');
+runTests(smallestCommonMultiplePrimes, 'smallestCommonMultiplePrimes');
+
+function runTests(smallestCommonMultiple, funcName) {
+  describe(funcName, () => {
+    let timings = [];
+    beforeEach(() => {
+      performance.clearMarks();
+      performance.mark('test-start');
+    });
+    afterEach(() => {
+      performance.mark('test-end');
+      timings.push(
+        performance.measure('test run', 'test-start', 'test-end').duration
+      );
+    });
+
+    after(async () => {
+      // console.log(timings);
+      await fs.appendFile(
+        'timings.txt',
+        `${funcName}\n\n${JSON.stringify(timings, null, 2)}\n\n`
+      );
+    });
+    it('returns the single number from a set of one', () => {
+      const testSet = [1];
+      expect(smallestCommonMultiple(testSet)).to.equal(1);
+    });
+    it('returns the scm >1 from natural numbers', () => {
+      const testSet = [2, 3];
+      expect(smallestCommonMultiple(testSet)).to.equal(6);
+    });
+    it('returns the scm of 2 prime numbers', () => {
+      const testSet = [3, 5];
+      expect(smallestCommonMultiple(testSet)).to.equal(15);
+    });
+    it('returns the scm of 2 non-prime numbers', () => {
+      const testSet = [4, 6];
+      expect(smallestCommonMultiple(testSet)).to.equal(12);
+    });
+    it('returns the scm of unsorted numbers', () => {
+      const testSet = [5, 3];
+      expect(smallestCommonMultiple(testSet)).to.equal(15);
+    });
+    it('returns the example case of 1 - 10', () => {
+      const testSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      expect(smallestCommonMultiple(testSet)).to.equal(2520);
+    });
+    it('solves 1 - 20', () => {
+      const testSet = Array.from({ length: 20 }, (_, i) => i + 1);
+      expect(smallestCommonMultiple(testSet)).to.equal(232792560);
+    }).timeout(10000);
+    it('solves 1 - 21', () => {
+      const testSet = Array.from({ length: 21 }, (_, i) => i + 1);
+      expect(smallestCommonMultiple(testSet)).to.equal(232792560);
+    }).timeout(10000);
+    it('solves large(ish) numbers', () => {
+      const testSet = [801, 970, 12345];
+      expect(smallestCommonMultiple(testSet)).to.equal(639446310);
+    }).timeout(10000);
   });
-  it('returns the first scm >1 from natural numbers', () => {
-    const testSet = [1, 2, 3];
-    expect(smallestCommonMultipleBrute(testSet)).to.equal(6);
-  });
-  it('returns the example case of 1 - 10', () => {
-    const testSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    expect(smallestCommonMultipleBrute(testSet)).to.equal(2520);
-  });
-  it('solves 1 - 20', () => {
-    const start = performance.mark('start');
-    const testSet = Array.from({ length: 20 }, (_, i) => i + 1);
-    const end = performance.mark('end');
-    expect(smallestCommonMultipleBrute(testSet)).to.equal(232792560);
-    // 1319ms on lecture mac
-    console.log(performance.measure('start to end', 'start'));
-  });
-});
+}
